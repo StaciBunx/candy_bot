2cef7f8ad0f9e82863344ec1c1ed98a7bf4caf93
Jeiojey <signofaries@gmail.com>
2022-10-08 22:47:12 +0300

    feature: all ready

--- main.py
+++ main.py
@@ -11,14 +11,13 @@ logging.basicConfig(filename='log.txt',
                     )
 logger = logging.getLogger(__name__)
 
-CANDY_QYT, MAX_PULL, FIRST_TURN, GAME = range(4)
+CANDY_QYT, FIRST_TURN, GAME = range(3)
 
 
 async def start(update, context):
     await update.message.reply_text(
         f'Привет, {update.effective_user.first_name}! Добро пожаловать в игру Candy Crush! '
-        'Правила: на столе лежат конфеты. Каждый ход соперники забирают себе некоторое количество. Тот, кто забирает со стола последнюю конфету - проигрывает партию. '
-        'Передумали играть? Нажмите /bye, чтобы закончить разговор.')
+        'Правила: на столе лежат конфеты. Каждый ход соперники забирают себе некоторое количество. Тот, кто забирает со стола последнюю конфету - проигрывает партию. Нажмите /bye, если передумали играть.')
     await update.message.reply_text(
         'Введите количество конфет в куче:')
     return CANDY_QYT
@@ -27,45 +26,47 @@ async def start(update, context):
 async def candy_qyt(update, context):
     candy_qyt = update.message.text
     if candy_qyt.isdigit():
-        candy_qyt = abs(int(candy_qyt))
+        context.user_data['candy_qyt'] = candy_qyt
         await update.message.reply_text(f'Хорошо, в кучке теперь {candy_qyt} конфет.')
         await update.message.reply_text(
             f'Введите максимальное количество конфет в раунд, которое можно вытянуть, но не больше {candy_qyt}:')
-        context.user_data['candy_qyt'] = candy_qyt
-        return MAX_PULL
+        return FIRST_TURN
     else:
         await update.message.reply_text("Вы ввели не число! Попробуйте еще раз:")
         return CANDY_QYT
 
 
-async def max_pull(update, context):
-    max_pull = update.message.text
-    await update.message.reply_text(f'Ок, вытягиваем не больше {max_pull} конфет.\n'
-                                    'Нажмите /next чтобы продолжить.')
-    context.user_data['max_pull'] = max_pull
-    return FIRST_TURN
-
-
 async def first_turn(update, context):
-    await update.message.reply_text('Сейчас выясним, кто ходит первый.')
-    await update.message.reply_text('Раз...')
-    await update.message.reply_text('Два...')
-    await update.message.reply_text('Три...')
-    turn = random.randint(1, 2)
-    if turn == 1:
-        await update.message.reply_text('Вы начинаете ход!\n'
-                                        'Введите, сколько вытянуть конфет:')
-        return GAME
+    max_pull = update.message.text
+    candy_qyt = int(context.user_data.get('candy_qyt'))
+    if max_pull.isdigit():
+        max_pull = int(max_pull)
+        if max_pull > candy_qyt:
+            await update.message.reply_text(f"Число не может быть больше {candy_qyt}. Попробуйте еще: ")
+            return FIRST_TURN
+        else:
+            context.user_data['max_pull'] = max_pull
+            await update.message.reply_text(f'Ок, вытягиваем не больше {max_pull} конфет.')
+            await update.message.reply_text('Сейчас выясним, кто ходит первый.')
+            await update.message.reply_text('Раз...')
+            await update.message.reply_text('Два...')
+            await update.message.reply_text('Три...')
+            turn = random.randint(1, 2)  # 1 - ход человека, 2 - ход бота
+            if turn == 1:
+                await update.message.reply_text('Вы начинаете ход!\n'
+                                                'Введите, сколько вытянуть конфет:')
+                return GAME
+            elif turn == 2:
+                num = random.randint(1, max_pull)
+                candy_qyt -= num
+                await update.message.reply_text(f"Ход начинает бот.")
+                await update.message.reply_text(f'Бот вытянул {num} конфет. В кучке остается {candy_qyt} конфет.')
+                await update.message.reply_text('Введите, сколько вытянуть конфет:')
+                context.user_data['candy_qyt'] = candy_qyt
+                return GAME
     else:
-        await update.message.reply_text(f"Ход начинает бот.")
-        candy_qyt = int(context.user_data.get('candy_qyt'))
-        max_pull = int(context.user_data.get('max_pull'))
-        num = random.randint(1, max_pull)
-        candy_qyt -= num
-        await update.message.reply_text(f'Бот вытягивает {num} конфет. В кучке остается {candy_qyt} конфет.')
-        await update.message.reply_text('Введите, сколько вытянуть конфет:')
-        context.user_data['candy_qyt'] = candy_qyt
-        return GAME
+        await update.message.reply_text("Вы ввели не число! Попробуйте еще раз:")
+        return FIRST_TURN
 
 
 async def game(update, context):
@@ -86,20 +87,25 @@ async def game(update, context):
         if candy_qyt > 0:
             await update.message.reply_text(f'Теперь в кучке {candy_qyt} конфет.')
             num = random.randint(1, max_pull)
+            if num > candy_qyt:
+                num = random.randint(1, candy_qyt)
             candy_qyt -= num
             context.user_data['candy_qyt'] = candy_qyt
-            await update.message.reply_text(f'Бот вытягивает {num} конфет. В кучке остается {candy_qyt} конфет.')
-            await update.message.reply_text('Введите, сколько вытянуть конфет:')
-            return GAME
-        elif candy_qyt <= 0:
-            await update.message.reply_text('В кучке не осталось конфет! Вы проиграли!')
+            if candy_qyt > 0:
+                await update.message.reply_text(f'Бот вытянул {num} конфет. В кучке остается {candy_qyt} конфет.')
+                await update.message.reply_text('Введите, сколько вытянуть конфет:')
+                return GAME
+            else:
+                await update.message.reply_text(f'Бот вытянул {num} конфет и конфет не осталось. Вы выиграли! Игра окончена. Нажмите /start, чтобы сыграть еще раз.')
+                return ConversationHandler.END
+        if candy_qyt <= 0:
+            await update.message.reply_text('В кучке не осталось конфет! Вы проиграли! Игра окончена. Нажмите /start, чтобы сыграть еще раз.')
             return ConversationHandler.END
 
 
 async def bye(update, context):
-    user = update.message.from_user
     await update.message.reply_text(
-        'Хорошо, тогда в другой раз!',
+        'Хорошо, пока!'
     )
     return ConversationHandler.END
 
@@ -109,12 +115,10 @@ app = ApplicationBuilder().token(TOKEN).build()
 game_conversation_handler = ConversationHandler(
     # точка входа в разговор
     entry_points=[CommandHandler('start', start)],
-    # этапы разговора, каждый со своим списком обработчиков сообщений
     states={
-        CANDY_QYT: [MessageHandler(filters.ALL, candy_qyt)],
-        MAX_PULL: [MessageHandler(filters.Regex(r'\d+'), max_pull)],
-        FIRST_TURN: [CommandHandler('next', first_turn)],
-        GAME: [MessageHandler(filters.Regex(r'\d+'), game)],
+        CANDY_QYT: [MessageHandler(~filters.COMMAND, candy_qyt)],
+        FIRST_TURN: [MessageHandler(filters.ALL, first_turn)],
+        GAME: [MessageHandler(filters.ALL, game)],
 
     },
     # точка выхода из разговора
